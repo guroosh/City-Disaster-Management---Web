acd83de18817a57e0e63fc8e703718466aaf0eb6
littl <littlethe2000@yahoo.com.tw>
2020-02-13 16:28:47 +0000

    initial gitlab push

---
+++ src/app/_models/user.ts
@@ -0,0 +1,14 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: string;
+    email: string;
+    phoneNumber: string;
+    govtIdNumber: string;
+    token: string;
+    isVolunteer : boolean;
+    volunteerType : string;
+}

8eceb8ae33e20672495a2536f7f82d506dde62db
littl <littlethe2000@yahoo.com.tw>
2020-02-27 18:03:22 +0000

    user list.

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -1,10 +1,12 @@
 ﻿export class AdminUser {
-    email: string;
-    firstName: string;
-    lastName: string;
-    BadgeId: string;
-    Role: string;
-    Department: string;
+    constructor(
+        public email: string,
+        public firstName: string,
+        public lastName: string,
+        public BadgeId: string,
+        public Role: string,
+        public Department: string
+    ){}
 }
 
 export class CommonUser {
--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,17 +1,39 @@
 import { Component} from '@angular/core';
+import { AdminUser } from '@/_models';
 
 @Component({
     selector: 'app-root',
     templateUrl: './userlist.component.html',
     //styleUrls: ['./map.component.css']
   })
+/*
+  function deleteUser2(){
+    alert('ooop');
+  }
+*/
   export class UserListComponent {
     title = 'userlist';
-    lat = 53.3498;
-    lng = -6.2603;
-    topic = 'desaster';
+
+    userList = [
+      new AdminUser('aaa','bbb','ccc','c','c','c'),
+      new AdminUser('aa333','bbb','ccc','c','c','c'),
+      new AdminUser('aaa','bb331b','ccc','c','c','c'),
+      new AdminUser('aaa','bbb','ccc','c','c','c000')
+    ];    
   
     constructor() {
+
     }
+
+    ngOnInit(){
+
+      function deleteUser(email){
+        alert('delete '+email);
+      };
+      function editUser(email){
+        alert('edit '+email);
+      };
+    }
+
   }
   

7570caa26e0e43097ee191063e351ff80fadff46
Manasi Palka <mpalkar27@gmail.com>
2020-04-21 01:50:06 +0100

    mp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent {
   menuList : menu[] = [ 
     {name:"Dashboard", icon:"menu", url:"/"},
     { name:"Disaster Report", icon:"menu", url:"/disasterReport"},
-    {name:"Registration", icon:"menu", url:"/"},
+    // {name:"Registration", icon:"menu", url:"/"},
     { name:"Rescue Team", icon:"menu", url:"/rescue"},
     { name:"Settings", icon:"settings", url:"/"},
     { name:"Logout", icon:"logout", url:"/"}]
